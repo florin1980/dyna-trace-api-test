8533abe16e9c00073d06c901dbed896cf6b0efaa
Florin Sandescu <florin.sandescu@kendart-systems.com>
2020-09-28 02:56:04 +0300

    #1 Cleanup library files

---
+++ update-mz.py
@@ -0,0 +1,109 @@
+import yaml, requests, ssl, json
+from random import randint
+
+global ENV, TENANT, TYPE
+TOKEN = 'JEJnQ4fjSWWBbU0UNXqJf'                                 # Config API Token
+HEADERS = {'Content-Type': 'application/json; charset=utf-8',
+           'Authorization': 'Api-Token ' + TOKEN}               # HEADERS & Dynatrace Config API Token for json payload validation
+
+yaml_data = dict()                                              # GLOBAL dict to receive YAML data
+rule_items = 'host-group-prefixes'                              # CONST YAML level entry for each new Dynatrace rule
+id_mz_length = 18                                               # CONST Management Zone ID length
+dyna_mz_data = []                                               # GLOBAL list to store existing Dynatrace IDs/Names
+dyna_mz_ids = []                                                # GLOBAL list to store Used MZ IDs (These are excepted while generating new (random) IDs
+
+
+# 1. open yaml file and get data
+def open_yaml():
+    global ENV, TENANT, TYPE
+    with open(r'desc-mz.yml') as file:
+        yaml_data.update(yaml.load(file, Loader=yaml.FullLoader))
+
+    ENV = str(yaml_data['enviromment_id'])                      # Base URL for JSON payload
+    TENANT = str(yaml_data['tenant_id'])                        # Florin Sandescu Trial Tenant ID
+    TYPE = str(yaml_data['type'])                               # Florin Sandescu Trial Tenant ID
+
+
+# 2. get current dynatrace list of IDs/Names
+def get_all_data_mz():
+    try:
+        # attempt to fetch all existing current Dyna dictionary (IDs/Names)
+        r = requests.get(ENV + '/api/config/v1/' + TYPE, headers=HEADERS)
+        print('%s save list: %d' % (TYPE, r.status_code))
+        res = r.json()
+
+        dyna_mz_data.append(res['values'])
+        for entry in res['values']:
+            dyna_mz_ids.append(entry['id'])
+
+    except ssl.SSLError:
+        print('SSL Error')
+
+
+# 3. Send PUT requests to Dyna Config API to update/create MZs
+def update_data_mz():
+    for name_mz, doc in yaml_data['teams'].items():
+
+        # get ID
+        id_mz = get_dyna_mz_id(name_mz)
+
+        # build JSON data / append rules according to YAML host-group-prefixes entries
+        data = {'name': name_mz, 'rules': list(), }
+        if rule_items in doc:
+            for host_group_prefix in doc[rule_items]:
+                data['rules'].append({
+                    'type': 'PROCESS_GROUP', 'enabled': 'true',
+                    'propagationTypes': ['PROCESS_GROUP_TO_SERVICE', 'PROCESS_GROUP_TO_HOST', ],
+                    'conditions': [{'key': {'attribute': 'HOST_GROUP_NAME', }, 'comparisonInfo': {'type': 'STRING', 'operator': 'BEGINS_WITH', 'value': host_group_prefix, 'negate': 'false', 'caseSensitive': 'true', }}, ]
+                })
+        json_data = json.dumps(data)
+
+        # UPDATE/CREATE existing/new Management Zone
+        try:
+            # attempt JSON request put
+            r = requests.put(
+                ENV + '/api/config/v1/' + TYPE + '/' + str(id_mz),
+                data=json_data, headers=HEADERS)
+            print('%s UPDATE/CREATE MZ with ID: %d' % (id_mz, r.status_code))
+
+            # add this ID to exception list
+            dyna_mz_ids.append(id_mz)
+
+        except ssl.SSLError:
+            print('SSL Error')
+
+
+# generate new ID for json request OR get existing ID
+def get_dyna_mz_id(name_mz):
+    r = ''
+
+    # get dyna ID if dyna name matches YAML name
+    s = r.join([i['id'] for i in dyna_mz_data[0] if i['name'] == name_mz])
+    # return random ID if above not found
+    s = new_random_id_mz() if len(s) == 0 else s
+    return s
+
+
+# generate random number of n digits as new IDs
+# excluding entries in dynatrace exception list
+def new_random_id_mz():
+    # set random number ranges as defined as CONST
+    # and generate random number
+    range_start = 10 ** (id_mz_length - 1)
+    range_end = (10 ** id_mz_length) - 1
+    i = randint(range_start, range_end)
+
+    # return random number NOT IN exception list IDs
+    return new_random_id_mz() if i in dyna_mz_ids else i
+
+
+# Main sequence execution
+def main():
+    open_yaml()
+    get_all_data_mz()
+    update_data_mz()
+
+
+# entry point
+if __name__ == '__main__':
+    main()
