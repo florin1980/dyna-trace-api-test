1d35856b37226f2ef5b9363ccd13a5960bff0708
flsandescu <florin.sandescu@kendart-systems.com>
2020-09-28 20:25:24 +0300

    #7 Encapsulate global variables, use hints, logger, major code changes

--- update-mz.py
+++ update-mz.py
@@ -1,127 +1,127 @@
-import yaml, requests, ssl, json
+import yaml, requests, ssl, json, logging
 from random import randint
 
-
-yaml_data = dict()                                              # GLOBAL dict to receive YAML data
-rule_items = 'host-group-prefixes'                              # CONST YAML level entry for each new Dynatrace rule
-id_mz_length = 18                                               # CONST Management Zone ID length
-dyna_mz_data = []                                               # GLOBAL list to store existing Dynatrace IDs/Names
-dyna_mz_ids = []                                                # GLOBAL list to store Used MZ IDs (These are excepted while generating new (random) IDs
+CFG_ENDPOINT = '/api/config/v1/'
+RULE_ITEMS = 'host-group-prefixes'
+ID_MZ_LENGTH = 18
 
 
-# 1. open yaml file and get data
-def open_yaml():
-    global ENV, TENANT, TYPE, TOKEN, HEADERS
-    with open(r'desc-mz.yml') as file:
-        yaml_data.update(yaml.load(file, Loader=yaml.FullLoader))
+class DynatraceAPI:
 
-    ENV = str(yaml_data['enviromment_id'])                      # Base URL for JSON payload
-    TENANT = str(yaml_data['tenant_id'])                        # Florin Sandescu Trial Tenant ID
-    TYPE = str(yaml_data['type'])                               # Florin Sandescu Trial Tenant ID
-    TOKEN = str(yaml_data['api_token'])                         # Config API Token
-    HEADERS = {'Content-Type': 'application/json; charset=utf-8',
-               'Authorization': 'Api-Token ' + TOKEN}               # HEADERS & Dynatrace Config API Token for json payload validation
+    def __init__(self):
+        # 1. open yaml file and get data
+        self.yaml_data = {}
+        with open(r'desc-mz.yml') as file:
+            self.yaml_data.update(yaml.load(file, Loader=yaml.FullLoader))
 
+        self.env_id = str(self.yaml_data['enviromment_id'])                             # Base URL for JSON payload
+        self.tenant = str(self.yaml_data['tenant_id'])                                  # Florin Sandescu Trial Tenant ID
+        self.type = str(self.yaml_data['type'])                                         # Florin Sandescu Trial Tenant ID
+        self.token = str(self.yaml_data['api_token'])                                   # Config API Token
+        self.headers = {'Content-Type': 'application/json; charset=utf-8',
+                        'Authorization': 'Api-Token ' + self.token}                     # HEADERS & Dynatrace Config API Token for json payload validation
+        self.dyna_mz_data = []
+        self.dyna_mz_ids = []
+        self.logger = logging.getLogger(__name__)
 
-# 2. get current dynatrace list of IDs/Names
-def get_current_data_mz():
+        # make it self.logger.debug to the console.
+        console = logging.StreamHandler()
+        self.logger.addHandler(console)
 
-    try:
-        # attempt to fetch all existing current Dyna dictionary (IDs/Names)
-        r = requests.get(ENV + '/api/config/v1/' + TYPE, headers=HEADERS)
-        print('%s save list: %d' % (TYPE, r.status_code))
-        res = r.json()
-
-        dyna_mz_data.append(res['values'])
-        for entry in res['values']:
-            dyna_mz_ids.append(entry['id'])
-
-    except ssl.SSLError:
-        print('SSL Error')
-
-
-# 3. Validate JSON payload prior to sending
-# ... as recommended here:
-# https://www.dynatrace.com/support/help/dynatrace-api/configuration-api/management-zones-api/put-mz/
-def validate_json_payload(id_mz, json_data):
-    try:
-        # attempt JSON request put
-        r = requests.post(
-            ENV + '/api/config/v1/' + TYPE + '/' + str(id_mz) + '/validator',
-            data=json_data, headers=HEADERS)
-        return r.status_code
-
-    except ssl.SSLError:
-        print('SSL Error')
-
-
-# 4. Send PUT requests to Dyna Config API to update/create MZs
-def update_data_mz():
-    for name_mz, doc in yaml_data['teams'].items():
-
-        # get ID
-        id_mz = get_dyna_mz_id(name_mz)
-
-        # build JSON data / append rules according to YAML host-group-prefixes entries
-        data = {'name': name_mz, 'rules': list(), }
-        if rule_items in doc:
-            for host_group_prefix in doc[rule_items]:
-                data['rules'].append({
-                    'type': 'PROCESS_GROUP', 'enabled': 'true',
-                    'propagationTypes': ['PROCESS_GROUP_TO_SERVICE', 'PROCESS_GROUP_TO_HOST', ],
-                    'conditions': [{'key': {'attribute': 'HOST_GROUP_NAME', }, 'comparisonInfo': {'type': 'STRING', 'operator': 'BEGINS_WITH', 'value': host_group_prefix, 'negate': 'false', 'caseSensitive': 'true', }}, ]
-                })
-        json_data = json.dumps(data)
-
-        # UPDATE/CREATE existing/new Management Zone
+    # 2. get current dynatrace list of IDs/Names
+    def get_current_data_mz(self):
         try:
-            # validate JSON request put
-            v = validate_json_payload(id_mz, json_data)
-            if v == 204:
-                # if valid attempt JSON request put
-                r = requests.put(
-                    ENV + '/api/config/v1/' + TYPE + '/' + str(id_mz),
-                    data=json_data, headers=HEADERS)
-                print('%s UPDATE/CREATE MZ with ID: %d' % (id_mz, r.status_code))
+            # attempt to fetch all existing current Dyna dictionary (IDs/Names)
+            r = requests.get(url=f'{self.env_id}{CFG_ENDPOINT}{self.type}', headers=self.headers)
+            self.logger.debug('%s save list: %d' % (self.type, r.status_code))
+            res = r.json()
 
-                # add this ID to exception list
-                dyna_mz_ids.append(id_mz)
-            else:
-                print('JSON payload Validation failed with status: ' + str(v))
+            self.dyna_mz_data.append(res['values'])
+            for entry in res['values']:
+                self.dyna_mz_ids.append(entry['id'])
 
         except ssl.SSLError:
-            print('SSL Error')
+            self.logger.debug('SSL Error')
 
+    # 3. Validate JSON payload prior to sending
+    # ... as recommended here:
+    # https://www.dynatrace.com/support/help/dynatrace-api/configuration-api/management-zones-api/put-mz/
+    def validate_json_payload(self, id_mz: str, json_data: str) -> int:
+        try:
+            # attempt JSON request put
+            r = requests.post(url=f'{self.env_id}{CFG_ENDPOINT}{self.type}/{id_mz}/validator',
+                              data=json_data, headers=self.headers)
+            return r.status_code
 
-# generate new ID for json request OR get existing ID
-def get_dyna_mz_id(name_mz):
-    r = ''
+        except ssl.SSLError:
+            self.logger.debug('SSL Error')
 
-    # get dyna ID if dyna name matches YAML name
-    s = r.join([i['id'] for i in dyna_mz_data[0] if i['name'] == name_mz])
-    # return random ID if above not found
-    s = new_random_id_mz() if len(s) == 0 else s
-    return s
+    # 4. Send PUT requests to Dyna Config API to update/create MZs
+    def update_data_mz(self) -> None:
+        for name_mz, doc in self.yaml_data['teams'].items():
 
+            # get ID
+            id_mz = self.get_dyna_mz_id(name_mz)
+            # build JSON data / append rules according to YAML host-group-prefixes entries
+            data = {'name': name_mz, 'rules': list(), }
+            if RULE_ITEMS in doc:
+                for host_group_prefix in doc[RULE_ITEMS]:
+                    data['rules'].append({
+                        'type': 'PROCESS_GROUP', 'enabled': 'true',
+                        'propagationTypes': ['PROCESS_GROUP_TO_SERVICE', 'PROCESS_GROUP_TO_HOST', ],
+                        'conditions': [{'key': {'attribute': 'HOST_GROUP_NAME', },
+                                        'comparisonInfo': {'type': 'STRING', 'operator': 'BEGINS_WITH',
+                                                           'value': host_group_prefix, 'negate': 'false',
+                                                           'caseSensitive': 'true', }}, ]
+                    })
+            json_data = json.dumps(data)
 
-# generate random number of n digits as new IDs
-# excluding entries in dynatrace exception list
-def new_random_id_mz():
-    # set random number ranges as defined as CONST
-    # and generate random number
-    range_start = 10 ** (id_mz_length - 1)
-    range_end = (10 ** id_mz_length) - 1
-    i = randint(range_start, range_end)
+            # UPDATE/CREATE existing/new Management Zone
+            try:
+                # validate JSON request put
+                v = self.validate_json_payload(id_mz, json_data)
+                if v == 204:
+                    # if valid attempt JSON request put
+                    r = requests.put(url=f'{self.env_id}{CFG_ENDPOINT}{self.type}/{id_mz}',
+                                     data=json_data, headers=self.headers)
+                    self.logger.debug('%s UPDATE/CREATE MZ with ID: %d' % (id_mz, r.status_code))
 
-    # return random number NOT IN exception list IDs
-    return new_random_id_mz() if i in dyna_mz_ids else i
+                    # add this ID to exception list
+                    self.dyna_mz_ids.append(id_mz)
+                else:
+                    self.logger.debug('JSON payload Validation failed with status: ' + str(v))
+
+            except ssl.SSLError:
+                self.logger.debug('SSL Error')
+
+    # generate new ID for json request OR get existing ID
+    def get_dyna_mz_id(self, name_mz: str) -> str:
+        r = ''
+        # get dyna ID if dyna name matches YAML name
+        s = r.join([i['id'] for i in self.dyna_mz_data[0] if i['name'] == name_mz])
+        # return random ID if above not found
+        s = self.new_random_id_mz() if len(s) == 0 else s
+        return str(s)
+
+    # generate random number of n digits as new IDs
+    # excluding entries in dynatrace exception list
+    def new_random_id_mz(self):
+        # set random number ranges as defined as CONST
+        # and generate random number
+
+        range_start = 10 ** (ID_MZ_LENGTH - 1)
+        range_end = (10 ** ID_MZ_LENGTH) - 1
+        i = randint(range_start, range_end)
+
+        # return random number NOT IN exception list IDs
+        return self.new_random_id_mz() if i in self.dyna_mz_ids else i
 
 
 # Main sequence execution
 def main():
-    open_yaml()
-    get_current_data_mz()
-    update_data_mz()
+    api = DynatraceAPI()
+    api.get_current_data_mz()
+    api.update_data_mz()
 
 
 # entry point
